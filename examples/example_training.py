fixed adagrad using the same hist_grad across the graph; added "random gradient" optimizer

examples/example_training.py
@@ -10,8 +10,11 @@ n_samples = 10
 itr = 1000
 
 ### data set
-x = np.ones((n_samples, 10))
-y = np.ones((n_samples, 5))
+x = np.random.random((n_samples, 10))
+y = np.zeros((n_samples, 10))
+# find the integer sum of the input and one-hot encode into y
+target_idx = np.floor(np.sum(x, axis = 1)).astype(int)
+y[np.arange(y.shape[0]), target_idx] = 1
 
 ### graph
 g = mf.Graph()
@@ -20,15 +23,15 @@ g = mf.Graph()
 x_placeholder = mf.placeholder([n_samples, 10], graph = g)
 
 ### NN layer
-W = mf.Variable(np.random.random((10, 5)), graph = g)
-b = mf.Variable(np.zeros((5)), graph = g) 
+W = mf.Variable(np.random.random((10, 10)), graph = g)
+b = mf.Variable(np.zeros((10)), graph = g) 
 
 ### activation
 z = mf.matmul(x_placeholder, W, graph = g) + b
 pred = mf.sigmoid(z, graph = g)
 
 ### cost function
-y_placeholder = mf.placeholder([n_samples, 5], graph = g)
+y_placeholder = mf.placeholder([n_samples, 10], graph = g)
 cost = mf.sum(pow((pred - y_placeholder), 2.0), graph = g) / (2 * n_samples)
 
 ### run the graph
@@ -41,8 +44,8 @@ print cost_res
 
 ### training step
 # compare the optimization methods
-#opt = mf.Train.GradientDescentOptimizer(0.1, graph = g.minimize(cost)
-opt = mf.Train.AdaGradOptimizer(0.1, graph = g).minimize(cost)
+#opt = mf.Train.GradientDescentOptimizer(0.8, graph = g).minimize(cost)
+opt = mf.Train.AdaGradOptimizer(0.8, graph = g).minimize(cost)
 
 for i in tqdm(range(itr)):
 	sess.run(opt, feed_dict = {x_placeholder: x, y_placeholder: y})

mlflow/Train/Optimizers.py
@@ -38,7 +38,7 @@ class GradientDescentOptimizer(Optimizer):
 		super(GradientDescentOptimizer, self).__init__(graph)
 		self.learning_rate = learning_rate
 
-	# optimzation strage
+	# optimzation stage
 	def opt(self, var, grad):
 		assert not grad is None
 		assert grad.shape == var.data.shape
@@ -50,20 +50,42 @@ class AdaGradOptimizer(Optimizer):
 		super(AdaGradOptimizer, self).__init__(graph)
 		self.learning_rate = learning_rate
 		self.eps = 1e-6
-		self.hist_grad = None
+		self.hist_grad = dict.fromkeys(self.opt_vars)
 
-	# optimzation strage
+	# optimzation stage
+	def opt(self, var, grad):
+		assert not grad is None
+		assert grad.shape == var.data.shape
+		assert var in self.hist_grad 
+
+		# first run, initialize historical grad (to 0)
+		if self.hist_grad[var] is None:
+			self.hist_grad[var] = np.zeros_like(grad)
+
+		# update historical gradients
+		self.hist_grad[var] += np.power(grad, 2)
+		# calculate the adjusted gradient
+		adj_grad = grad / (self.eps + np.sqrt(self.hist_grad[var]))
+
+		var.update(var.data - adj_grad * self.learning_rate)
+
+
+### This optimizer is implemented just for fun,
+### it's goal is to introduce a new regularization 
+### technique. 
+### Be cautious about using it until I test
+### it's effectiveness. 
+class RandomGradientOptimizer(Optimizer):
+	def __init__(self, learning_rate, skip_rate, graph):
+		super(RandomGradientOptimizer, self).__init__(graph)
+		self.learning_rate = learning_rate
+		self.skip_rate = min(max(skip_rate, 0.0), 1.0)
+
+	# optimzation stage
 	def opt(self, var, grad):
 		assert not grad is None
 		assert grad.shape == var.data.shape
 
-		# first run, initialize historical grad (to 0)
-		if self.hist_grad is None:
-			self.hist_grad = np.zeros_like(grad)
-
-		# update historical gradients
-		self.hist_grad += np.power(grad, 2)
-		# calculate the adjusted gradient
-		adj_grad = grad / (self.eps + np.sqrt(self.hist_grad))
-
-		var.update(var.data - adj_grad * self.learning_rate)
+		# only optimize a few variables based on skip rate
+		if np.random.random() > self.skip_rate:
+			var.update(var.data - grad * self.learning_rate)